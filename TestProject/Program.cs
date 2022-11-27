f9b62de0e876f50db65a3d9aaee092712f054621
Denny Peltz <_denny@msn.com>
2022-11-26 16:59:59 -0800

    - Select colors that resemble resistor code colors 1-9 - Add a test project to illustrate the effect

---
+++ TestProject/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ranbow Braces Test Project");
+Console.WriteLine("Load project in Visual Studio to illustrate");
+
+// Colors begin repeating after Cycle length value (range: 1-9)
+// Tools -> Options -> Environment -> Fonts and Colors -> Rainbow Braces -> Cycle length
+
+Array<bool> level = new(20);
+
+if (level[1])
+{
+    if (level[2])
+    {
+        if (level[3])
+        {
+            if (level[4])
+            {
+                if (level[5])
+                {
+                    if (level[6])
+                    {
+                        if (level[7])
+                        {
+                            if (level[8])
+                            {
+                                if (level[9])
+                                {
+                                    if (level[10])
+                                    {
+                                        if (level[11])
+                                        {
+                                            if (level[12])
+                                            {
+                                                if (level[13])
+                                                {
+                                                    if (level[14])
+                                                    {
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
