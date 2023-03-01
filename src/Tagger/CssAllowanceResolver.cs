455b2f011f401ca5c7eb96a6bdfe7e0c946c7340
Ondřej Kubíček <lordfanger@gmail.com>
2023-03-01 19:05:02 +0100

    Colorize parentheses and square brackets is CSS Property Value
    Fixes: #60

---
+++ src/Tagger/CssAllowanceResolver.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+
+namespace RainbowBraces.Tagger
+{
+    public class CssAllowanceResolver : AllowanceResolver
+    {
+        protected override TagAllowance IsAllowed(IClassificationType tagType)
+        {
+            // Allow for CSS Property Value tag
+            if (tagType.IsOfType("CSS Property Value")) return TagAllowance.Punctuation;
+
+            // Allow for CSS Selector
+            if (tagType.IsOfType("CSS Selector")) return TagAllowance.Operator;
+            return TagAllowance.Disallowed;
+        }
+
+        protected override TagAllowance IsAllowed(ILayeredClassificationType layeredType)
+        {
+            string classification = layeredType.Classification;
+
+            // Allow for CSS Property Value tag
+            if (classification == "CSS Property Value") return TagAllowance.Punctuation;
+
+            // Allow for CSS Selector
+            if (classification == "CSS Selector") return TagAllowance.Operator;
+            return TagAllowance.Disallowed;
+        }
+    }
+}
