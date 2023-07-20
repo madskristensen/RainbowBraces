3f2459208eb4e346dd024d5b6bc8ba34754934da
Ondřej Kubíček <lordfanger@gmail.com>
2023-07-20 16:36:50 +0200

    Support for diff editor

--- src/Helper/CustomTextViewRoles.cs
+++ src/Helper/CustomTextViewRoles.cs
@@ -6,5 +6,16 @@
         /// TextView role for sticky scroll container.
         /// </summary>
         public const string StickyScroll = "STICKYSCROLL_TEXT_VIEW";
+
+        /// <summary>
+        /// TextView role for diff editor.
+        /// All diff windows share this role.
+        /// </summary>
+        public const string Diff = "DIFF";
+
+        /// <summary>
+        /// TextView role for inline diff editor.
+        /// </summary>
+        public const string InlineDiff = "INLINEDIFF";
     }
 }
